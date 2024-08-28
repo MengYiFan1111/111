6bf5320bd809712462da4cc97726fe9865e51d26
meng <15893810750@163.com>
2024-08-28 10:43:59 +0800

    添加项目框架

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Student {
+    private String name;
+    private int age;
+    private String major;
+    private String grade;
+    private String studentID;
+    private String email;
+}
